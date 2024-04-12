61e4ea164e9b375434b35ee4b7da0a4f75f95547
mallika <mallika.nilns@gmail.com>
2024-04-12 19:16:16 +0700

    fix: category post form

--- src/app/admin/components/post-category/post-category.component.ts
+++ src/app/admin/components/post-category/post-category.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { AdminService } from '../../service/admin.service';
+import { log } from 'console';
 
 @Component({
   selector: 'app-post-category',
@@ -26,23 +27,28 @@ export class PostCategoryComponent {
     })
   }
 
-  // addCategory(): void {
-  //   if (this.categoryForm.valid) {
-  //     this.adminService.addCategory(this.categoryForm.value).subscribe((res) => {
-  //       if (res.id != null) {
-  //         this.snackBar.open('Category Posted Successfully!', 'Close', {
-  //           duration: 5000
-  //         });
-  //         this.router.navigateByUrl('/admin/dashboard');
-  //       } else {
-  //         this.snackBar.open(res.message, 'Close', {
-  //           duration: 5000,
-  //           panelClass: 'error-snackbar'
-  //         });
-  //       }
-  //     })
-  //   } else {
-  //     this.categoryForm.markAllAsTouched();
-  //   }
-  // }
+  addCategory(): void {
+    if (this.categoryForm.valid) {
+      console.log('test0');
+      this.adminService.addCategory(this.categoryForm.value).subscribe((res) => {
+        console.log('test1');
+        if (res.id != null) {
+          console.log('test2');
+          this.snackBar.open('Category Posted Successfully!', 'Close', {
+            duration: 5000
+          });
+          this.router.navigateByUrl('/admin/dashboard');
+        } else {
+          console.log('test3');
+          this.snackBar.open(res.message, 'Close', {
+            duration: 5000,
+            panelClass: 'error-snackbar'
+          });
+        }
+      })
+    } else {
+      console.log('test4');
+      this.categoryForm.markAllAsTouched();
+    }
+  }
 }
